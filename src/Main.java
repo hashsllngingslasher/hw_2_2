b09cdb8e6647ad142aca02ae9a925fc1d952b9ee
hashsllngingslasher <erkin.seitbekov@gmail.com>
2023-08-09 18:51:48 +0600

    up2

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        Main main = new Main();
-        MusicPlayer element1 = main.createObject("Artist");
-        MusicPlayer element2 = main.createObject("Song");
-        MusicPlayer element3 = main.createObject("Genre");
-
-        Printable[] elements = {element1, element2, element3};
+        Printable[] elements = {createObject("Artist"), createObject("Song"), createObject("Genre")};
         System.out.println("------MY PLAYLIST------");
         for (Printable element : elements) {
             element.print();
         }
     }
 
-    public MusicPlayer createObject(String className) {
-        switch (className) {
-            case "Artist":
-                return new Artist("Oasis", 23);
-            case "Song":
-                return new Song("Wonderwall", 3.21);
-            case "Genre":
-                return new Genre("Rock");
-            default:
-                throw new IllegalArgumentException("Недопустимый класс: " + className);
-        }
+    public static MusicPlayer createObject(String className) {
+        return switch (className) {
+            case "Artist" -> new Artist("Oasis", 23);
+            case "Song" -> new Song("Wonderwall", 3.21);
+            case "Genre" -> new Genre("Rock");
+            default -> throw new IllegalArgumentException("Недопустимый класс: " + className);
+        };
     }
 }
